ef15676d19cfc365d2d9edaa9a222ede774210aa
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2020-12-28 21:59:48 +0500

    Cors

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+const cors = require('./cors');
+
 
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/',authenticate.verifyUser ,authenticate.verifyAdmin,function(req, res, next) {
+router.get('/',cors.corsWithOptions, authenticate.verifyUser ,authenticate.verifyAdmin,function(req, res, next) {
   // res.send('respond with a resource');
   User.find({}, function(err, result) {
     if (err) {
@@ -19,7 +21,7 @@ router.get('/',authenticate.verifyUser ,authenticate.verifyAdmin,function(req, r
   });
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup',cors.corsWithOptions,  (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -49,7 +51,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login',cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
