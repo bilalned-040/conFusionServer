b1c52d8762a609340164a0986774aefac12e2192
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2020-12-29 16:43:29 +0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '880964915974246',
+        clientSecret: '2322b14a5a71059d50b6155a708faca3'
+    }
 }
